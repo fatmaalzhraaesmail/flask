c8821e396ab997ac1d9838781a801d0be8aa465a
fatmaalzhraaesmail <72276464+fatmaalzhraaesmail@users.noreply.github.com>
2023-05-20 04:28:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -118,9 +118,9 @@ def upload_file():
 #     return result
 # return None
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+# if __name__ == '__main__':
+#     app.run(debug=True, port=os.getenv("PORT", default=5000))
